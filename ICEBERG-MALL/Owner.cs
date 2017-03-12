ffbc64733089f132c227bb80968dd99ddfc8ba1f
AnastasiyaZelenova <zelenova.nastya@icloud.com>
2017-03-13 00:28:15 +0300

    Добавление некоторых окон, создание сущностей, определение свойств и полей.

---
+++ ICEBERG-MALL/Owner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICEBERG_MALL
+{
+    class Owner
+    {
+        private string _nameOwner;
+
+        public string NameOwner
+        {
+            get { return _nameOwner; }
+            set { _nameOwner = value; }
+        }
+
+        private int _numberOwner;
+
+        public int NumberOwner
+        {
+            get { return _numberOwner; }
+            set { _numberOwner = value; }
+        }
+
+        public Owner (string nameOwner, int numberOwner)
+        {
+            _nameOwner = nameOwner;
+            _numberOwner = numberOwner;
+        }
+
+    }
+}
---
+++ ICEBERG-MALL/TradePoint.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICEBERG_MALL
+{
+    class TradePoint
+    {
+        private string _typeTradePoint;
+
+        public string TypeTradePoint
+        {
+            get { return _typeTradePoint; }
+            set { _typeTradePoint = value; }
+        }
+
+        private string _nameTradePoint;
+
+        public string NameTradePoint
+        {
+            get { return _nameTradePoint; }
+            set { _nameTradePoint = value; }
+        }
+
+        private int _numberTradePoint;
+
+        public int NumberTradePoint
+        {
+            get { return _numberTradePoint; }
+            set { _numberTradePoint = value; }
+        }
+
+        private DateTime _openingHours;
+
+        public DateTime OpeningHours
+        {
+            get { return _openingHours; }
+            set { _openingHours = value; }
+        }
+
+        private int _floor;
+
+        public int Floor
+        {
+            get { return _floor; }
+            set { _floor = value; }
+        }
+
+        public TradePoint (string typeTradePoint, string nameTradePoint, int numberTradePoint, DateTime openingHours, int floor)
+        {
+            _typeTradePoint = TypeTradePoint;
+            _nameTradePoint = NameTradePoint;
+            _numberTradePoint = NumberTradePoint;
+            _openingHours = OpeningHours;
+            _floor = Floor;
+        }
+
+
+    }
+}
